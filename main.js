516876ab4ba6611690491c2d91cf5af15c0a9a67
Invdigitales <97065304+Invdigitales@users.noreply.github.com>
2024-07-01 18:45:40 -0300

    cinco

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ boton.addEventListener("click", () => {
       icon: 'success',
       html:
         'INGRESÁ' +
-        '' +
+        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3278.328166449957!2d-56.4461346!3d-34.7473253!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a1c45efac80efb%3A0x2d97f6253f8d9ada!2sGranja%20Tur%C3%ACstica%20Las%20Cabras!5e0!3m2!1ses-419!2suy!4v1719869792083!5m2!1ses-419!2suy" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
         'Podes ampliar el mapa',
         
         
